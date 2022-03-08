ef2694d60b95d48d04a59850eea6d408eea1182d
Nate <36239250+RaidenXP@users.noreply.github.com>
2022-03-08 11:59:15 -0800

    tried out some ways to visualize genre count

--- data_vis_2.py
+++ data_vis_2.py
@@ -1,11 +1,22 @@
-from turtle import pos, position
 import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
 
 def genre_analysis():
     steam_data = pd.read_csv('steam_games')
 
     genre_occurences = (steam_data['genre'].value_counts()).to_dict()
+
+    genre_df = pd.DataFrame(genre_occurences.keys(), columns=['genre'])
+    count_df = pd.DataFrame(genre_occurences.values(), columns=['count'])
+
+    new_df = pd.merge(genre_df, count_df, left_index=True, right_index=True)
+
+    #plt.bar(genre_occurences.keys(), genre_occurences.values())
+
+    sns.catplot(x='genre', data=steam_data, kind='count')
+
+    plt.show()
 
 if __name__ == "__main__":
     genre_analysis()
